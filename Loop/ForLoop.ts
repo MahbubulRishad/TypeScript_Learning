c29f5465de2f2879bd99be52aaff9f0bd987bb9b
MahbubulRishad <mahbubulrishad@gmail.com>
2025-08-27 14:32:42 +0600

    For Loop

---
+++ Loop/ForLoop.ts
@@ -0,0 +1,17 @@
+// for loop
+
+for (var i: number=1; i<=10;i++){
+    console.log(i);
+}
+
+console.log("-----------");
+
+for (var j:number = 10; j>0;j--){
+    console.log(j);
+}
+
+console.log("-----------");
+
+for (var k:number=1; k<=50; k+=2){
+    console.log(k);
+}
